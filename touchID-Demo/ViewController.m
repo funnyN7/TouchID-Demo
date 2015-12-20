5bfef1a6b59cd86985b55f48f6eae3d5a7294229
yq616775291 <616775291@qq.com>
2015-12-20 13:42:56 +0800

    ADD CODE

--- touchID-Demo/ViewController.m
+++ touchID-Demo/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import <LocalAuthentication/LocalAuthentication.h>
 @interface ViewController ()
-
+@property (strong,nonatomic) LAContext * context;
 @end
 
 @implementation ViewController
@@ -22,6 +22,41 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)touchID:(id)sender
+{
+    self.context = [LAContext new];
+    
+    NSError *error;
+    _context.localizedFallbackTitle = @"";
+    
+    if ([_context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error])
+    {
+        NSLog(@"Touch ID is available.");
+        [_context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+                localizedReason:NSLocalizedString(@"Use Touch ID to log in.", nil)
+                          reply:^(BOOL success, NSError *error) {
+                              if (success)
+                              {
+                                  NSLog(@"Authenticated using Touch ID.");
+                              }
+                              else
+                              {
+                                  if (error.code == kLAErrorUserFallback) {
+                                      NSLog(@"User tapped Enter Password");
+                                  } else if (error.code == kLAErrorUserCancel) {
+                                      NSLog(@"User tapped Cancel");
+                                  } else {
+                                      NSLog(@"Authenticated failed.");
+                                  }
+                              }}];
+    }
+         
+         else {
+             
+             NSLog(@"Touch ID is not available: %@", error);
+             
+         }
 }
 
 @end
